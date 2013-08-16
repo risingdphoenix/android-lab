cf35339a3aa754747c69b9e08babc8a9257dd235
risingdphoenix <risingphoenix.a@gmail.com>
2013-08-16 17:06:25 +0700

    delete all

--- BasicOOPProject/BasicOOP/build/source/buildConfig/debug/oop/basic/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package oop.basic;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-
-}
--- BasicOOPProject/BasicOOP/src/main/java/oop/basic/model/GetCreatedTimes.java
+++
@@ -1,9 +0,0 @@
-package oop.basic.model;
-
-/**
- * Created by USER on 9/8/2556.
- */
-public interface GetCreatedTimes {
-    public int getAllClassCreatedTimes();
-    public int getClassCreatedTimes();
-}
